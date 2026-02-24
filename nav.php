91dc3c397880cb393011b4c189da0eafee1ad1d4
pjailani558615-cmyk <p.jailani.558615@umindanao.edu.ph>
2026-02-24 10:45:28 +0800

    new update

--- nav.php
+++ nav.php
@@ -2,7 +2,7 @@
 <div style="display:flex; gap:10px; flex-wrap:wrap; margin-bottom:16px;">
   <a href="/assessment_beginner/index.php">Dashboard</a>
   <a href="clients_list.php">Clients</a>
-  <a href="/assessment_beginner/pages/services_list.php">Services</a>
+  <a href="services_list.php">Services</a>
   <a href="/assessment_beginner/pages/bookings_list.php">Bookings</a>
   <a href="/assessment_beginner/pages/tools_list_assign.php">Tools</a>
   <a href="/assessment_beginner/pages/payments_list.php">Payments</a>
---
+++ services_list.php
@@ -0,0 +1,32 @@
+<?php
+include "db.php";
+$stats = getStats($conn);
+$services = $stats['services'];
+$result = mysqli_query($conn, "SELECT * FROM services ORDER BY service_id DESC");
+?>
+<!doctype html>
+<html>
+<head><meta charset="utf-8"><title>Services</title>
+<link rel="stylesheet" href="color.css">
+</head>
+<body>
+<?php include "nav.php"; ?>
+ 
+<h2>Services</h2>
+<p>Total Services: <b><?php echo $services; ?></b></p>
+<table border="1" cellpadding="8">
+  <tr>
+    <th>ID</th><th>Name</th><th>Rate</th><th>Active</th><th>Action</th>
+  </tr>
+  <?php while($row = mysqli_fetch_assoc($result)) { ?>
+    <tr>
+      <td><?php echo $row['service_id']; ?></td>
+      <td><?php echo $row['service_name']; ?></td>
+      <td>₱<?php echo number_format($row['hourly_rate'],2); ?></td>
+      <td><?php echo $row['is_active'] ? "Yes" : "No"; ?></td>
+      <td><a href="services_edit.php?id=<?php echo $row['service_id']; ?>">Edit</a></td>
+    </tr>
+  <?php } ?>
+</table>
+</body>
+</html>
